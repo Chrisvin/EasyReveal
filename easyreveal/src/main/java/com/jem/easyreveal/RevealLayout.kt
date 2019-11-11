90128d56c2f387fe4da686327d6e7a2d1bbe4fb4
Jem <chrisvin1997@gmail.com>
2019-11-12 01:12:13 +0530

    Change RevealLayout interface hide/reveal fn arguments

--- easyreveal/src/main/java/com/jem/easyreveal/RevealLayout.kt
+++ easyreveal/src/main/java/com/jem/easyreveal/RevealLayout.kt
@@ -1,10 +1,12 @@
 package com.jem.easyreveal
 
+import android.animation.ValueAnimator
+
 internal interface RevealLayout {
     // Used to perform reveal animation
-    public fun reveal(): Unit
+    public fun reveal(onUpdate:((it: ValueAnimator) -> Unit)? = null): Unit
     // Used to perform hide animation
-    public fun hide(): Unit
+    public fun hide(onUpdate:((it: ValueAnimator) -> Unit)? = null): Unit
 
     // Reveal view to specified reveal percentage
     public fun revealForPercentage(percent: Float): Unit
