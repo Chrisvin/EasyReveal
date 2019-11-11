5f0f111f70555c712fc7e39d1109bd77e75a92c3
Jem <chrisvin1997@gmail.com>
2019-11-12 03:25:06 +0530

    Update demo app

--- app/src/main/java/com/example/easyrevealdemo/MainActivity.kt
+++ app/src/main/java/com/example/easyrevealdemo/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.easyrevealdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.jem.easyreveal.ClipPathProvider
+import com.jem.easyreveal.clippathproviders.CircularClipPathProvider
+import com.jem.easyreveal.clippathproviders.LinearClipPathProvider
+import com.jem.easyreveal.clippathproviders.SweepClipPathProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +16,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.clipPathProviderArray,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            clipPathProvideSpinner.adapter = adapter
+            clipPathProvideSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    revealLayout.clipPathProvider = clipPathProviderArrayList[position].apply {
+                        if (this is CircularClipPathProvider) {
+                            when (position) {
+                                4 -> setCircleCenter(revealLayout.width.toFloat()/2, revealLayout.height.toFloat()/2)
+                                5 -> setCircleCenter(revealLayout.width.toFloat()/2, 0f)
+                                6 -> setCircleCenter(0f, revealLayout.height.toFloat())
+                            }
+                        } else if (this is SweepClipPathProvider) {
+                            when (position) {
+                                7 -> setCircleCenter(revealLayout.width.toFloat()/2, revealLayout.height.toFloat()/2)
+                                8 -> setCircleCenter(revealLayout.width.toFloat()/2, revealLayout.height.toFloat())
+                                9 -> setCircleCenter(0f, 0f)
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         revealLayout.setOnClickListener {
             if (flag) {
@@ -22,4 +63,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var flag = true
+    private val clipPathProviderArrayList = arrayListOf<ClipPathProvider>(
+        LinearClipPathProvider(LinearClipPathProvider.Direction.LEFT_TO_RIGHT),
+        LinearClipPathProvider(LinearClipPathProvider.Direction.RIGHT_TO_LEFT),
+        LinearClipPathProvider(LinearClipPathProvider.Direction.TOP_TO_BOTTOM),
+        LinearClipPathProvider(LinearClipPathProvider.Direction.BOTTOM_TO_TOP),
+        CircularClipPathProvider(),
+        CircularClipPathProvider(),
+        CircularClipPathProvider(),
+        SweepClipPathProvider(),
+        SweepClipPathProvider(),
+        SweepClipPathProvider()
+    )
 }
