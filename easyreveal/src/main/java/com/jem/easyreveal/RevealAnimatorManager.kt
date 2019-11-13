b8f8e538194fb0179a8f5338d68f3cfa9ae7a074
Jem <chrisvin1997@gmail.com>
2019-11-14 03:12:19 +0530

    Refactor RevealAnimatorManager

--- easyreveal/src/main/java/com/jem/easyreveal/RevealAnimatorManager.kt
+++ easyreveal/src/main/java/com/jem/easyreveal/RevealAnimatorManager.kt
@@ -1,32 +1,20 @@
 package com.jem.easyreveal
 
 import android.animation.ValueAnimator
+import kotlin.math.abs
 
 internal class RevealAnimatorManager {
     // Animator used for animating the reveal/hide animations
     private val revealAnimator: ValueAnimator = ValueAnimator()
 
-    public fun reveal(duration: Long, onUpdate: ((it: ValueAnimator) -> Unit)? = null) {
+    public fun animate(startPercent: Float, endPercent: Float, duration: Long, onUpdate: ((it: ValueAnimator) -> Unit)? = null) {
         if (revealAnimator.isRunning) {
             val temp = revealAnimator.animatedValue as Float
-            revealAnimator.setFloatValues(temp, 100f)
-            revealAnimator.duration = (duration * ((100f - temp) / 100f)).toLong()
+            revealAnimator.setFloatValues(temp, endPercent)
+            revealAnimator.duration = (duration * (abs(endPercent - temp) / 100f)).toLong()
         } else {
-            revealAnimator.setFloatValues(0f, 100f)
-            revealAnimator.duration = duration
-        }
-        revealAnimator.addUpdateListener(onUpdate)
-        revealAnimator.start()
-    }
-
-    public fun hide(duration: Long, onUpdate: ((it: ValueAnimator) -> Unit)? = null) {
-        if (revealAnimator.isRunning) {
-            val temp = revealAnimator.animatedValue as Float
-            revealAnimator.setFloatValues(temp, 0f)
-            revealAnimator.duration = duration - (duration * ((100f - temp) / 100f)).toLong()
-        } else {
-            revealAnimator.setFloatValues(100f, 0f)
-            revealAnimator.duration = duration
+            revealAnimator.setFloatValues(startPercent, endPercent)
+            revealAnimator.duration = (duration * (abs(endPercent - startPercent) / 100f)).toLong()
         }
         revealAnimator.addUpdateListener(onUpdate)
         revealAnimator.start()
